a2fff66770dca0fd5e0341afb7765856d54d5dcd
Титов Дима <dima.titov18@gmail.com>
2020-02-21 14:52:08 +0300

    Добавлен glLookat, убрана возможность менять угол системы координат,  добавлена возможность перемещения камеры, а не системы координат

--- NoMinecraft/NoMinecraft/src/Main.cpp
+++ NoMinecraft/NoMinecraft/src/Main.cpp
@@ -5,65 +5,69 @@
 #include <glut.h>
 
 float View = 45; // угол обзора
-int WindW, WindH;
+int WindW = 1280, WindH = 720; // –азмер окна
 int i;
-int alpha;
 int rotate_y = 0;
 int rotate_x = 0;
-float cube_size = 0.1;
-float translate_z = 0;
-float translate_x = 0;
-int quantity_cube_x = 1;
-int quantity_cube_y = 1;
-
-void processNormalKeys(unsigned char key, int x, int y)
-{
-    if (key == 'w') translate_z += 0.01;
-    if (key == 's') translate_z -= 0.01;
-
-    if (key == 'a') translate_x += 0.05;
-    if (key == 'd') translate_x -= 0.05;
-    
-    if (key == '-')View += 5;
-    if (key == '+')View -= 5;
-
-    if (View > 90) View = 90;
-    if (View < 15) View = 15;
+float cube_size = 1.0;
+int quantity_cube_x = 5;
+int quantity_cube_y = 5;
+float distance_between_cubs = cube_size*2;
+float PlayerX = 0.0, PlayerY = 1.0, PlayerZ = 0.0; // координаты камеры
+float lx = 0.0f, lz = -1.0f; // координаты вектора направлени€ движени€ камеры
+float angle = 0.0; // угол поворота камеры
 
 
-}
 
 
-void specialKeys(int key, int x, int y) {
+void specialKeys(int key, int x, int y) 
+{   
+    float fraction = 0.1f;
+    if (key == GLUT_KEY_RIGHT)   rotate_y += 5;
+    else if (key == GLUT_KEY_LEFT)  rotate_y -= 5;
 
-    //  Right arrow - increase rotation by 5 degree
-    if (key == GLUT_KEY_RIGHT)
-        rotate_y += 5;
 
-    //  Left arrow - decrease rotation by 5 degree
-    else if (key == GLUT_KEY_LEFT)
-        rotate_y -= 5;
+    if (key == GLUT_KEY_UP) rotate_x -= 5;
+    else if (key == GLUT_KEY_DOWN)  rotate_x += 5;
 
-    if (key == GLUT_KEY_UP)
-        rotate_x -= 5;
-
-    else if (key == GLUT_KEY_DOWN)
-        rotate_x += 5;
+    if (key == GLUT_ACTIVE_SHIFT) PlayerY += 0.5;
+    if (key == GLUT_ACTIVE_CTRL) PlayerY -= 0.5;
 
     if (rotate_x > 90) rotate_x = 90;
     if (rotate_x < -90) rotate_x = -90;
 
+    
+    /*switch (key) {
+        case GLUT_KEY_LEFT:
+            angle -= 0.01f;
+            lx = sin(angle);
+            lz = -cos(angle);
+            break;
+
+        case GLUT_KEY_RIGHT:
+            angle += 0.01f;
+            lx = sin(angle);
+            lz = -cos(angle);
+           break;
+
+       case GLUT_KEY_UP:
+           x += lx * fraction;
+           z += lz * fraction;
+          break;
+
+       case GLUT_KEY_DOWN:
+           x -= lx * fraction;
+           z -= lz * fraction;
+           break;
+        case 
+    }*/
 
     //  Request display update
     glutPostRedisplay();
-
 }
 
 void Reshape(int w, int h) // Reshape function
 {
-    // предотвращение делени€ на ноль
-    if (h == 0)
-        h = 1;
     float ratio = w * 1.0 / h;
     // используем матрицу проекции
     glMatrixMode(GL_PROJECTION);
@@ -77,87 +81,124 @@ void Reshape(int w, int h) // Reshape function
     glMatrixMode(GL_MODELVIEW);
 }
 
+void processNormalKeys(unsigned char key, int x, int y)
+{
+    if (key == 'w') PlayerZ += 0.5;
+    if (key == 's') PlayerZ -= 0.5;
+
+    if (key == 'a') PlayerX += 0.5;
+    if (key == 'd') PlayerX -= 0.5;
+
+    if (key == '-') 
+    {
+        distance_between_cubs += 0.05;
+    }
+    if (key == '+')
+    {
+        distance_between_cubs -= 0.05;
+    }
+    if (View > 90) View = 90;
+    if (View < 15) View = 15;
+}
+
+void Draw_cube() {
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex3f(cube_size, -cube_size, cube_size);
+    glVertex3f(cube_size, cube_size, cube_size);
+    glVertex3f(-cube_size, cube_size, cube_size);
+    glVertex3f(-cube_size, -cube_size, cube_size);
+    glEnd();
+
+    // ‘иолетова€ сторона Ч ѕ–ј¬јя
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 0.0, 1.0);
+    glVertex3f(cube_size, -cube_size, -cube_size);
+    glVertex3f(cube_size, cube_size, -cube_size);
+    glVertex3f(cube_size, cube_size, cube_size);
+    glVertex3f(cube_size, -cube_size, cube_size);
+    glEnd();
+
+    // «елена€ сторона Ч Ћ≈¬јя
+    glBegin(GL_POLYGON);
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(-cube_size, -cube_size, cube_size);
+    glVertex3f(-cube_size, cube_size, cube_size);
+    glVertex3f(-cube_size, cube_size, -cube_size);
+    glVertex3f(-cube_size, -cube_size, -cube_size);
+    glEnd();
+
+    // —ин€€ сторона Ч ¬≈–’Ќяя
+    glBegin(GL_POLYGON);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(cube_size, cube_size, cube_size);
+    glVertex3f(cube_size, cube_size, -cube_size);
+    glVertex3f(-cube_size, cube_size, -cube_size);
+    glVertex3f(-cube_size, cube_size, cube_size);
+    glEnd();
+
+    //  расна€ сторона Ч Ќ»∆Ќяя
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(cube_size, -cube_size, -cube_size);
+    glVertex3f(cube_size, -cube_size, cube_size);
+    glVertex3f(-cube_size, -cube_size, cube_size);
+    glVertex3f(-cube_size, -cube_size, -cube_size);
+    glEnd();
+
+    //  расна€ сторона Ч передн€€
+    glBegin(GL_POLYGON);
+    glColor3f(cube_size, 1.0, cube_size);
+    glVertex3f(cube_size, -cube_size, -cube_size);
+    glVertex3f(cube_size, cube_size, -cube_size);
+    glVertex3f(-cube_size, cube_size, -cube_size);
+    glVertex3f(-cube_size, -cube_size, -cube_size);
+    glEnd();
+}
+
 void Draw(void) // Window redraw function
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
     glPushMatrix();
-    //glRotatef(alpha, 1.1f, 0.9f, 1.0f);
-    //alpha += 4;
-    //if (alpha > 359) alpha = 0
+    //glRotatef(rotate_x, 1.0, 0.0, 0.0); // поворот системы кординат по x
+    //glRotatef(rotate_y, 0.0, 1.0, 0.0);// поворот системы кординат по y
+    //glTranslatef(PlayerX, 0.0, PlayerZ);
 
-    glRotatef(rotate_x, 1.0, 0.0, 0.0);
-    glRotatef(rotate_y, 0.0, 1.0, 0.0);
-    glTranslatef(translate_x, 0.0, translate_z);
+    gluLookAt(PlayerX,          PlayerY,    PlayerZ,
+              PlayerX + lx,     0.0f,       PlayerZ + lz,
+              0.0f,             1.0f,       0.0f            );
 
-    // Ѕела€ сторона Ч «јƒЌяя
-    for (float i = 0; i < cube_size * quantity_cube_x; i += cube_size * 2)
+
+    for (int i = -4; i < 4; i++)// рисуем кубы сеткой 8х8
+        for (int j = -4; j < 4; j++)
+        {
+            glPushMatrix();
+            glTranslatef(i * distance_between_cubs, -0.2, j * distance_between_cubs);
+            Draw_cube();
+            glPopMatrix();
+        }
+    
+   
+    /*for (float i = 0; i < cube_size * quantity_cube_x; i += cube_size * 2)
     {
         for (float j = 0; j < cube_size * quantity_cube_y; j += cube_size * 2)
         {
+            glPushMatrix();
+            glTranslatef(i, 5, j);
+            Draw_cube();
+            glTranslatef(-i, -5, -j);
 
-            glTranslatef(i, 0.0, j);
+            gluLookAt(0.0f, 0.0f, 10.0f,
+                0.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f);
 
-            glBegin(GL_POLYGON);
-            glColor3f(1.0, 1.0, 1.0);
-            glVertex3f(cube_size, -cube_size, cube_size);
-            glVertex3f(cube_size, cube_size, cube_size);
-            glVertex3f(-cube_size, cube_size, cube_size);
-            glVertex3f(-cube_size, -cube_size, cube_size);
-            glEnd();
-
-            // ‘иолетова€ сторона Ч ѕ–ј¬јя
-            glBegin(GL_POLYGON);
-            glColor3f(1.0, 0.0, 1.0);
-            glVertex3f(cube_size, -cube_size, -cube_size);
-            glVertex3f(cube_size, cube_size, -cube_size);
-            glVertex3f(cube_size, cube_size, cube_size);
-            glVertex3f(cube_size, -cube_size, cube_size);
-            glEnd();
-
-            // «елена€ сторона Ч Ћ≈¬јя
-            glBegin(GL_POLYGON);
-            glColor3f(0.0, 1.0, 0.0);
-            glVertex3f(-cube_size, -cube_size, cube_size);
-            glVertex3f(-cube_size, cube_size, cube_size);
-            glVertex3f(-cube_size, cube_size, -cube_size);
-            glVertex3f(-cube_size, -cube_size, -cube_size);
-            glEnd();
-
-            // —ин€€ сторона Ч ¬≈–’Ќяя
-            glBegin(GL_POLYGON);
-            glColor3f(0.0, 0.0, 1.0);
-            glVertex3f(cube_size, cube_size, cube_size);
-            glVertex3f(cube_size, cube_size, -cube_size);
-            glVertex3f(-cube_size, cube_size, -cube_size);
-            glVertex3f(-cube_size, cube_size, cube_size);
-            glEnd();
-
-            //  расна€ сторона Ч Ќ»∆Ќяя
-            glBegin(GL_POLYGON);
-            glColor3f(1.0, 0.0, 0.0);
-            glVertex3f(cube_size, -cube_size, -cube_size);
-            glVertex3f(cube_size, -cube_size, cube_size);
-            glVertex3f(-cube_size, -cube_size, cube_size);
-            glVertex3f(-cube_size, -cube_size, -cube_size);
-            glEnd();
-
-            //  расна€ сторона Ч передн€€
-            glBegin(GL_POLYGON);
-            glColor3f(cube_size, 1.0, cube_size);
-            glVertex3f(cube_size, -cube_size, -cube_size);
-            glVertex3f(cube_size, cube_size, -cube_size);
-            glVertex3f(-cube_size, cube_size, -cube_size);
-            glVertex3f(-cube_size, -cube_size, -cube_size);
-            glEnd();
-            glTranslatef(-i, 0.0, -j);
-
+            glPopMatrix();
         }
-    }
+    }*/
+
 
     glPopMatrix();
-
-    glFlush();
+    glFinish();
     glutSwapBuffers();
 }
 
@@ -174,9 +215,7 @@ void timf(int value) // Timer function
 
 int main(int argc, char* argv[])
 {
-    WindW = 1000;
-    WindH = 1000;
-    alpha = 0;
+    
 
     glutInit(&argc, argv);
     glutInitWindowSize(WindW, WindH);
@@ -188,7 +227,7 @@ int main(int argc, char* argv[])
     glutDisplayFunc(Draw);    // Set up redisplay function 
     glutTimerFunc(40, timf, 0); // Set up timer for 40ms, about 25 fps
     glutVisibilityFunc(Visibility); // Set up visibility funtion
-    glClearColor(0, 128, 255, 100);
+    //glClearColor(0, 128, 255, 100);
     glutSpecialFunc(specialKeys);
     glutKeyboardFunc(processNormalKeys);
 
